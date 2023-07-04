ecd420d1f195ae35bb4b5776b062d59d4a5f124f
s2rgi0 <tlakolu@gmail.com>
2023-07-04 16:19:01 -0600

    refactor after JAXB generated classes

---
+++ spring5-mvc-rest/src/main/java/guru/springfamework/api/v1/mapper/CustomerMapper.java
@@ -0,0 +1,17 @@
+package guru.springfamework.api.v1.mapper;
+
+
+import guru.springfamework.domain.Customer;
+import guru.springframework.model.CustomerDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CustomerMapper {
+
+    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+
+    CustomerDTO customerToCustomerDTO(Customer customer);
+
+    Customer customerDtoToCustomer(CustomerDTO customerDTO);
+}
